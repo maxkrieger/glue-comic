87c46da29ccc8063d243c8a7f8c5294e66edcceb
maxkrieger <themaxaxis@gmail.com>
2020-01-26 18:14:49 -0500

    Updates

---
+++ component---src-pages-page-2-js-5b53894b5a2bbbd8470f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"To be added"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-5b53894b5a2bbbd8470f.js.map
