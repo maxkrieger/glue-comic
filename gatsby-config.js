3b270672ff7cb7fea1a11e7ff8737a0e726fc2e7
maxkrieger <themaxaxis@gmail.com>
2020-01-31 22:53:31 -0500

    add/clean sources, favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#614AEC`,
         theme_color: `#614AEC`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/citations.js
+++ src/pages/citations.js
@@ -11,6 +11,28 @@ const Citation = ({ link, children }) => (
   </li>
 )
 
+const IMAGE_CITATIONS = [
+  "https://commons.wikimedia.org/wiki/File:ELIZA_conversation.jpg",
+  "https://commons.wikimedia.org/wiki/File:Eugene_de_Blaas_-_Conversation_on_the_terrace,_Venice.jpg",
+  "https://commons.wikimedia.org/wiki/File:Ikekichi_-_A_Conversation_-_Walters_9598.jpg",
+  "https://commons.wikimedia.org/wiki/File:Penguin_Conversation_(1186088012)_(2).jpg",
+  "http://www.dubberly.com/category/topics/conversation-topics",
+  "https://commons.wikimedia.org/wiki/File:Telegraph_-_relay.jpg",
+  "https://commons.wikimedia.org/wiki/File:Transcription_using_cylinder_phonograph.png",
+  "https://en.wikipedia.org/wiki/File:Romanian_pickup1.jpg",
+  "https://commons.wikimedia.org/wiki/File:Comparison_of_Full-Keyboard,_Single-Shift,_and_Double-Shift_Typerwriters_in_1911.png",
+  "https://commons.wikimedia.org/wiki/File:King_Malcolms_Gravestone_at_Glamis.jpg",
+  "https://commons.wikimedia.org/wiki/File:Sketchnote.jpg",
+  "https://commons.wikimedia.org/wiki/File:Class_Notes_Islam.jpg",
+  "https://commons.wikimedia.org/wiki/File:Parse_thicket_constructed_from_parse_trees_for_sentences.jpg",
+  "https://en.wikipedia.org/wiki/File:Graphophone1901.jpg",
+  "https://commons.wikimedia.org/wiki/File:The_care_of_trees_in_lawn,_street_and_park,_with_a_list_of_trees_and_shrubs_for_decorative_use_(1910)_(20621549638).jpg",
+  "https://commons.wikimedia.org/wiki/File:White_Matter_Connections_Obtained_with_MRI_Tractography.png",
+  "https://commons.wikimedia.org/wiki/File:BOSS_Great_Wall.jpg",
+  "https://williambasinski.bandcamp.com/album/the-disintegration-loops",
+  "https://www.youtube.com/watch?v=VlSkPA60ujQ",
+]
+
 const CitationsPage = () => (
   <Layout>
     <SEO title="Citations" />
@@ -21,22 +43,27 @@ const CitationsPage = () => (
         J. R. Anderson, “A spreading activation theory of memory,” Journal of
         Verbal Learning and Verbal Behavior, vol. 22, no. 3, pp. 261–295, 1983.
       </Citation>
+      <Citation link="http://www.dubberly.com/articles/what-is-conversation.html">
+        Dubberly, Hugh, and Paul Pangaro. “What Is Conversation? How Can We
+        Design for Effective Conversation?” Dubberly Design Office, 1 May 2009
+      </Citation>
+      <Citation>
+        Carse, James. Finite and Infinite Games a Vision of Life as Play and
+        Possibility. Free Press, 2012.
+      </Citation>
     </ul>
     <h3>Images</h3>
     <ul>
-      <li>
-        https://commons.wikimedia.org/wiki/File:The_care_of_trees_in_lawn,_street_and_park,_with_a_list_of_trees_and_shrubs_for_decorative_use_(1910)_(20621549638).jpg
-      </li>
-      <li>
-        https://commons.wikimedia.org/wiki/File:White_Matter_Connections_Obtained_with_MRI_Tractography.png
-      </li>
-      <li>https://commons.wikimedia.org/wiki/File:BOSS_Great_Wall.jpg</li>
+      {IMAGE_CITATIONS.map(image => (
+        <a href={image}>
+          <li>{image}</li>
+        </a>
+      ))}
     </ul>
     <h2>Prior Work</h2>
     <ul>
       <Citation link="http://www.graycrawford.com/nonlinear-conversational-medium">
         G. Crawford, “Nonlinear Conversational Medium,” Nov-2017. [Online].
-        Available: http://www.graycrawford.com/nonlinear-conversational-medium.
       </Citation>
     </ul>
     <h3>Comic Inspiration</h3>
@@ -46,8 +73,7 @@ const CitationsPage = () => (
         2015.
       </Citation>
       <Citation link="http://elzr.com/else/napkin-comic.pdf">
-        E. Parra, “Napkin,” 25-Apr-2014. [Online]. Available:
-        http://elzr.com/else/napkin-comic.pdf.
+        E. Parra, “Napkin,” 25-Apr-2014. [Online].
       </Citation>
       <Citation>Dynamicland Zine</Citation>
     </ul>
